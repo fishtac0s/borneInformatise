5fc350e4fe5b5fa7392bb61c42a6a4fd402003a3
fishTac0s <e.clagace5@etu.cvm.qc.ca>
2022-09-27 17:08:19 -0400

    ajout information

---
+++ Parking/CarteCredit.java
@@ -0,0 +1,19 @@
+package Parking;
+
+public class CarteCredit {
+    //variable
+    public double solde = 100;
+    public int numeroCarte;
+    public int expiration;
+
+
+    public CarteCredit(){
+        this.numeroCarte = numeroCarte;
+        this.expiration = expiration;
+
+    }
+
+    public int getExpiration() {return expiration;}
+
+    public int getNumeroCarte() {return numeroCarte;}
+}
---
+++ Parking/Transaction.java
@@ -0,0 +1,16 @@
+package Parking;
+
+public class Transaction {
+    private String codeStationnement;
+
+
+    public Transaction(){
+        this.codeStationnement = codeStationnement;
+    }
+
+    public String getCodeStationnement() {return codeStationnement;}
+
+    public void verificationCodeStatonnement(){
+
+    }
+}
